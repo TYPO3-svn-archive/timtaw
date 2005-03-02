f54bcd66c81e9afbb05ed8dc51a55678c9879e6a
sebastian <sebastian@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-03-02 14:21:48 +0000

    ---------------------------------------------------------------------- updated patch
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/timtaw/trunk@2613 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/class.tx_timtaw_login.php
+++ res/class.tx_timtaw_login.php
@@ -39,7 +39,8 @@ class tx_timtaw_login {
 	 * Main function, registering itself as a hook
 	 */
 	function loginBackendUser($params, $parent = '')	{
-		require_once ($params['PATH_t3lib'].'class.t3lib_befunc.php');
+		echo PATH_t3lib;
+		require_once (PATH_t3lib.'class.t3lib_befunc.php');
 		
 			// log out user if logged in when needed
 		if($_COOKIE['be_typo_user']) {
